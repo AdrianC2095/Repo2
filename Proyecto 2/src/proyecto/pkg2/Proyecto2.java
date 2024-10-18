dfd57005a182039dc55ea22ebad39bd2696e7ac1
AdrianC2095 <adriancuenca095@gmail.com>
2024-10-18 17:31:13 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto 2/src/proyecto/pkg2/Proyecto2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg2;
+
+/**
+ *
+ * @author INTEL
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo 2 Básico de Java Versionado a través de Git Manejado desde GitHub");
+        System.out.println("Además se USAR GitHubClassroom");
+    }
+    
+}
